1ea2c86389a43aee366c3553bacad4c8664ab01b
Abdulsalam Aderoju <aderojuabdulsalam15@gmail.com>
2025-12-19 16:25:59 +0100

    Render db

--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-DATABASE_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/hack_health"
+# DATABASE_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/hack_health"
+DATABASE_URL = "postgresql://medisense_db_8n2b_user:eouQxUfjR8XSbTWZEt4N9Eo8eNZla0kb@dpg-d52mqg6mcj7s73bs29jg-a/medisense_db_8n2b"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
